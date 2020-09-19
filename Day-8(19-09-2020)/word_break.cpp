dd1854c285371f1fbf8a5c47b3a7c5615453b8d6
suryatejabandlamudi <suryatejabandlamudi@gmail.com>
2020-09-19 12:46:31 +0530

    Day 8

--- Day-8(19-09-2020)/word_break.cpp
+++ Day-8(19-09-2020)/word_break.cpp
@@ -8,13 +8,18 @@ public:
         for(string str: wordDict)
             dict.insert(str);
         
+        int longestWord = 0;
+        for(string word : dict){
+            longestWord = max(longestWord, (int)word.size());
+        }
+        
         vector<bool> dp(s.length() + 1, false);
         dp[0] = true;
         string tmp;
         
         for(int i=1; i<=s.length(); i++)
         {
-            for(int j=i-1; j>=0; j--)
+            for(int j=i-1; j>=max(i-longestWord,0); j--)
             {
                 if(dp[j])
                 {
